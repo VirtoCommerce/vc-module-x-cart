4c20c88b78626505e9e0e0a276be442b32da50b0
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-07-15 15:18:49 +0200

    feat: add overloaded method to validation context factory

--- src/VirtoCommerce.XCart.Core/Validators/CartValidationContextFactory.cs
+++ src/VirtoCommerce.XCart.Core/Validators/CartValidationContextFactory.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VirtoCommerce.XCart.Core.Models;
 using VirtoCommerce.XCart.Core.Services;
 
 namespace VirtoCommerce.XCart.Core.Validators
@@ -26,6 +28,21 @@ namespace VirtoCommerce.XCart.Core.Validators
             {
                 CartAggregate = cartAggregate,
                 AllCartProducts = cartProductsTask.Result,
+                AvailPaymentMethods = availPaymentsTask.Result,
+                AvailShippingRates = availShippingRatesTask.Result,
+            };
+        }
+
+        public async Task<CartValidationContext> CreateValidationContextAsync(CartAggregate cartAggregate, IList<CartProduct> products)
+        {
+            var availPaymentsTask = _availMethods.GetAvailablePaymentMethodsAsync(cartAggregate);
+            var availShippingRatesTask = _availMethods.GetAvailableShippingRatesAsync(cartAggregate);
+            await Task.WhenAll(availPaymentsTask, availShippingRatesTask);
+
+            return new CartValidationContext
+            {
+                CartAggregate = cartAggregate,
+                AllCartProducts = products,
                 AvailPaymentMethods = availPaymentsTask.Result,
                 AvailShippingRates = availShippingRatesTask.Result,
             };
