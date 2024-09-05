bfbcda9863beb6f6cdca58186d084a18612bcf1f
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-09-05 15:36:42 +0200

    feat: prepare shipment middleware

---
+++ src/VirtoCommerce.XCart.Core/Models/ShipmentContextCartMap.cs
@@ -0,0 +1,11 @@
+﻿using VirtoCommerce.CartModule.Core.Model;
+
+namespace VirtoCommerce.XCart.Core.Models
+{
+    public class ShipmentContextCartMap
+    {
+        public CartAggregate CartAggregate { get; set; }
+
+        public Shipment Shipment { get; set; }
+    }
+}
