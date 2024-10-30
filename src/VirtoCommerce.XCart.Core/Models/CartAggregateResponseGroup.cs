51dc87b7574b4f0dbb902ec40fceb6a9c350ed9c
Oleg Zhuk <zhukoo@gmail.com>
2024-10-30 10:30:15 +0200

    Revert back CartAggregateResponseGroup enum items

--- src/VirtoCommerce.XCart.Core/Models/CartAggregateResponseGroup.cs
+++ src/VirtoCommerce.XCart.Core/Models/CartAggregateResponseGroup.cs
@@ -6,12 +6,12 @@ namespace VirtoCommerce.XCart.Core.Models
     [Flags]
     public enum CartAggregateResponseGroup
     {
-        Default = 0,
+        None = 0,
         WithPayments = 1,
         WithLineItems = 1 << 1,
         WithShipments = 1 << 2,
-        WithDynamicProperties = 1 << 3,
-        RecalculateTotals = 1 << 4,
-        Full = Default | WithPayments | WithLineItems | WithShipments | WithDynamicProperties | RecalculateTotals
+        Validate = 1 << 3,
+        WithDynamicProperties = 1 << 4,
+        Full = WithPayments | WithLineItems | WithShipments | Validate | WithDynamicProperties
     }
 }
