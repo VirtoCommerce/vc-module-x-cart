2daf0f79eb14fb9d800a69ea28a35112dcd8b5c9
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-11-20 17:44:56 +0200

    fix: sonar

--- src/VirtoCommerce.XCart.Core/Schemas/ConfigurationQueryResponseType.cs
+++ src/VirtoCommerce.XCart.Core/Schemas/ConfigurationQueryResponseType.cs
@@ -13,19 +13,3 @@ public class ConfigurationQueryResponseType : ExtendableGraphType<ProductConfigu
             resolve: context => context.Source.ConfigurationSections);
     }
 }
-
-public class ConfigurationSectionType : ExtendableGraphType<ExpProductConfigurationSection>
-{
-    public ConfigurationSectionType()
-    {
-        Field(x => x.Id, nullable: false).Description("Configuration section id");
-        Field(x => x.Name, nullable: true).Description("Configuration section name");
-        Field(x => x.Type, nullable: true).Description("Configuration section type");
-        Field(x => x.Description, nullable: true).Description("Configuration section description");
-        Field(x => x.IsRequired, nullable: false).Description("Is configuration section required");
-
-        ExtendableField<ListGraphType<ConfigurationLineItemType>>(
-            nameof(ExpProductConfigurationSection.Options),
-            resolve: context => context.Source.Options);
-    }
-}
---
+++ src/VirtoCommerce.XCart.Core/Schemas/ConfigurationSectionType.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Types;
+using VirtoCommerce.Xapi.Core.Schemas;
+using VirtoCommerce.XCart.Core.Models;
+
+namespace VirtoCommerce.XCart.Core.Schemas;
+
+public class ConfigurationSectionType : ExtendableGraphType<ExpProductConfigurationSection>
+{
+    public ConfigurationSectionType()
+    {
+        Field(x => x.Id, nullable: false).Description("Configuration section id");
+        Field(x => x.Name, nullable: true).Description("Configuration section name");
+        Field(x => x.Type, nullable: true).Description("Configuration section type");
+        Field(x => x.Description, nullable: true).Description("Configuration section description");
+        Field(x => x.IsRequired, nullable: false).Description("Is configuration section required");
+
+        ExtendableField<ListGraphType<ConfigurationLineItemType>>(
+            nameof(ExpProductConfigurationSection.Options),
+            resolve: context => context.Source.Options);
+    }
+}
