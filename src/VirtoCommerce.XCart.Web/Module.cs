95209de1a31f2b8d53e5b7e4c1d0552d1313e583
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-01-21 13:19:12 +0200

    feat: move settings from xApi

--- src/VirtoCommerce.XCart.Web/Module.cs
+++ src/VirtoCommerce.XCart.Web/Module.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.Xapi.Core.Extensions;
 using VirtoCommerce.XCart.Core;
 using VirtoCommerce.XCart.Data;
@@ -28,6 +30,13 @@ public class Module : IModule, IHasConfiguration
     public void PostInitialize(IApplicationBuilder appBuilder)
     {
         appBuilder.UseScopedSchema<DataAssemblyMarker>("cart");
+
+        var serviceProvider = appBuilder.ApplicationServices;
+
+        // settings
+        var settingsRegistrar = serviceProvider.GetRequiredService<ISettingsRegistrar>();
+        settingsRegistrar.RegisterSettings(ModuleConstants.Settings.General.AllSettings, ModuleInfo.Id);
+        settingsRegistrar.RegisterSettingsForType(ModuleConstants.Settings.StoreLevelSettings, nameof(Store));
     }
 
     public void Uninstall()
