205a3c5d56d872643b23ff80613d833a66a8d08c
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-01-23 18:15:37 +0200

    fix: obsolete warnings

--- src/VirtoCommerce.XCart.Core/Schemas/InputAddItemType.cs
+++ src/VirtoCommerce.XCart.Core/Schemas/InputAddItemType.cs
@@ -21,7 +21,7 @@ namespace VirtoCommerce.XCart.Core.Schemas
             // Configurable product support
             Field<ListGraphType<ConfigurationSectionInput>>("configurationSections");
 
-            Field<DateTimeGraphType>("createdDate", "Create date. Optional, to manually control line item position in the cart if required. ISO-8601 format, for example: 2025-01-23T11:46:11Z");
+            Field<DateTimeGraphType>("createdDate").Description("Create date. Optional, to manually control line item position in the cart if required. ISO-8601 format, for example: 2025-01-23T11:46:11Z");
         }
     }
 }
