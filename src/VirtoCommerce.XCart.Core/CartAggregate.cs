8d91ea908ade2dedb87b2079543be5aa79924be1
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-01-31 15:30:23 +0200

    feat: replace all references of product name with localized one

--- src/VirtoCommerce.XCart.Core/CartAggregate.cs
+++ src/VirtoCommerce.XCart.Core/CartAggregate.cs
@@ -341,7 +341,7 @@ namespace VirtoCommerce.XCart.Core
                         result.Sku = product.Product.Code;
                         result.ImageUrl ??= product.Product.ImgSrc;
                         result.MeasureUnit ??= product.Product.MeasureUnit;
-                        result.Name ??= product.Product.Name;
+                        result.Name ??= product.GetName(Cart.LanguageCode);
                     }
 
                     var giftInCart = GiftItems.FirstOrDefault(x => x.EqualsReward(result));
