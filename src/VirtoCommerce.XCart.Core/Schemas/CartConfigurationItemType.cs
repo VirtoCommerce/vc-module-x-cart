bd4db74a76a882682bb39b99ee0320651f572c6b
Evgenii Kolosov <kolosov.e.s@gmail.com>
2025-03-06 11:50:11 +0100

    VCST-2565: sectionId is required

--- src/VirtoCommerce.XCart.Core/Schemas/CartConfigurationItemType.cs
+++ src/VirtoCommerce.XCart.Core/Schemas/CartConfigurationItemType.cs
@@ -10,7 +10,7 @@ namespace VirtoCommerce.XCart.Core.Schemas
         {
             Field(x => x.Id, nullable: false).Description("Configuration item ID");
             Field(x => x.Name, nullable: true).Description("Configuration item name");
-            Field(x => x.SectionId, nullable: true).Description("Configuration item section ID");
+            Field(x => x.SectionId, nullable: false).Description("Configuration item section ID");
             Field(x => x.ProductId, nullable: true).Description("Configuration item product ID");
             Field(x => x.Quantity, nullable: true).Description("Configuration item product quantity");
             Field(x => x.CustomText, nullable: true).Description("Custom text for 'Text'-type configuration item section");
