93e11d7f8203af22e2f734e8f4e90409b5a0917b
Basil Kotov <basilkot@gmail.com>
2025-03-28 10:28:02 +0000

    remove unnecessary interface inheritance

--- src/VirtoCommerce.XCart.Core/Queries/GetPickupLocationsQuery.cs
+++ src/VirtoCommerce.XCart.Core/Queries/GetPickupLocationsQuery.cs
@@ -1,10 +1,9 @@
 using VirtoCommerce.Xapi.Core.BaseQueries;
-using VirtoCommerce.Xapi.Core.Infrastructure;
 using VirtoCommerce.XCart.Core.Models;
 
 namespace VirtoCommerce.XCart.Core.Queries;
 
-public class GetPickupLocationsQuery : SearchQuery<PickupLocationsResponse>, ISearchQuery
+public class GetPickupLocationsQuery : SearchQuery<PickupLocationsResponse>
 {
     public string StoreId { get; set; }
 }
