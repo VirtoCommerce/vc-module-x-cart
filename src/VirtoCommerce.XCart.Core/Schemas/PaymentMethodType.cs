e3147d7810b30a04b68768c5e6223269987f7b8b
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-06-19 09:21:06 +0200

    VCST-3376: Add localized name to PaymentMethodType (#51)
    Co-authored-by: Oleg Zhuk <zhukoo@gmail.com>
    Co-authored-by: Elena Mutykova <56412054+Lenajava1@users.noreply.github.com>

--- src/VirtoCommerce.XCart.Core/Schemas/PaymentMethodType.cs
+++ src/VirtoCommerce.XCart.Core/Schemas/PaymentMethodType.cs
@@ -1,5 +1,7 @@
+using GraphQL;
 using GraphQL.Types;
 using VirtoCommerce.PaymentModule.Core.Model;
+using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Xapi.Core.Extensions;
 using VirtoCommerce.Xapi.Core.Schemas;
 using VirtoCommerce.XCart.Core.Extensions;
@@ -11,11 +13,14 @@ namespace VirtoCommerce.XCart.Core.Schemas
         public PaymentMethodType()
         {
             Field(x => x.Code, nullable: false).Description("Value of payment gateway code");
-            Field(x => x.Name, nullable: true).Description("Value of payment method name");
             Field(x => x.Description, nullable: true).Description("Payment method description");
             Field(x => x.LogoUrl, nullable: true).Description("Value of payment method logo absolute URL");
             Field(x => x.Priority, nullable: false).Description("Value of payment method priority");
             Field(x => x.IsAvailableForPartial, nullable: false).Description("Is payment method available for partial payments");
+
+            Field<StringGraphType>("name")
+                .Resolve(context => GetLocalizedValue(context, context.Source.LocalizedName, context.Source.Name))
+                .Description("Localized name of payment method.");
 
             Field<NonNullGraphType<CurrencyType>>("currency")
                 .Description("Currency")
@@ -57,6 +62,22 @@ namespace VirtoCommerce.XCart.Core.Schemas
             Field<NonNullGraphType<StringGraphType>>("paymentMethodGroupType")
                 .Description("Value of payment group type")
                 .Resolve(context => context.Source.PaymentMethodGroupType.ToString());
+        }
+
+        private static string GetLocalizedValue(IResolveFieldContext context, LocalizedString localizedString, string fallbackValue = null)
+        {
+            var cultureName = context.GetArgumentOrValue<string>("cultureName");
+
+            if (!string.IsNullOrEmpty(cultureName))
+            {
+                var localizedValue = localizedString?.GetValue(cultureName);
+                if (!string.IsNullOrEmpty(localizedValue))
+                {
+                    return localizedValue;
+                }
+            }
+
+            return fallbackValue;
         }
     }
 }
