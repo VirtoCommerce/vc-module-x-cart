7bd907b869b12ac40f0893b810e0ca1b62aa81d5
yuskithedeveloper <yuskianywhere@gmail.com>
2025-07-22 11:39:17 -0500

    Refactoring: QueryBuilder authorization

--- src/VirtoCommerce.XCart.Data/Queries/GetSavedForLaterListQueryBuilder.cs
+++ src/VirtoCommerce.XCart.Data/Queries/GetSavedForLaterListQueryBuilder.cs
@@ -4,28 +4,44 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using VirtoCommerce.Xapi.Core.BaseQueries;
 using VirtoCommerce.Xapi.Core.Extensions;
-using VirtoCommerce.Xapi.Core.Services;
+using VirtoCommerce.Xapi.Core.Security.Authorization;
 using VirtoCommerce.XCart.Core;
 using VirtoCommerce.XCart.Core.Queries;
 using VirtoCommerce.XCart.Core.Schemas;
+using VirtoCommerce.XCart.Data.Authorization;
 
 namespace VirtoCommerce.XCart.Data.Queries;
 
-public class GetSavedForLaterListQueryBuilder(IMediator mediator, IAuthorizationService authorizationService, IUserManagerCore userManagerCore)
+public class GetSavedForLaterListQueryBuilder(IMediator mediator, IAuthorizationService authorizationService)
     : QueryBuilder<GetSavedForLaterListQuery, CartAggregate, CartType>(mediator, authorizationService)
 {
     protected override string Name => "getSavedForLater";
+
+    protected override GetSavedForLaterListQuery GetRequest(IResolveFieldContext<object> context)
+    {
+        var result = base.GetRequest(context);
+
+        result.UserId = context.GetCurrentUserId();
+        result.OrganizationId = context.GetCurrentOrganizationId();
+
+        return result;
+    }
 
     protected override async Task BeforeMediatorSend(IResolveFieldContext<object> context, GetSavedForLaterListQuery request)
     {
         await base.BeforeMediatorSend(context, request);
 
-        await userManagerCore.CheckCurrentUserState(context, allowAnonymous: false);
+        if (!context.IsAuthenticated())
+        {
+            throw AuthorizationError.AnonymousAccessDenied();
+        }
     }
 
     protected override async Task AfterMediatorSend(IResolveFieldContext<object> context, GetSavedForLaterListQuery request, CartAggregate response)
     {
         await base.AfterMediatorSend(context, request, response);
+
+        await Authorize(context, response.Cart, new CanAccessCartAuthorizationRequirement());
 
         context.SetExpandedObjectGraph(response);
     }
