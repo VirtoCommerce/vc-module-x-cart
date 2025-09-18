1861ba595f1f419170a4ce3fbea4504e9e2f666a
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-09-18 08:47:09 +0200

    VCST-3870: Fix NRE in configurationItems query (#82)

--- src/VirtoCommerce.XCart.Data/Queries/GetConfigurationItemsQueryBuilder.cs
+++ src/VirtoCommerce.XCart.Data/Queries/GetConfigurationItemsQueryBuilder.cs
@@ -30,7 +30,7 @@ public class GetConfigurationItemsQueryBuilder : QueryBuilder<GetConfigurationIt
 
         var cartAuthorizationRequirement = new CanAccessCartAuthorizationRequirement();
 
-        if (response is not null)
+        if (response is not null && response.CartAggregate is not null)
         {
             await Authorize(context, response.CartAggregate.Cart, cartAuthorizationRequirement);
         }
@@ -40,7 +40,11 @@ public class GetConfigurationItemsQueryBuilder : QueryBuilder<GetConfigurationIt
 
     protected override Task AfterMediatorSend(IResolveFieldContext<object> context, GetConfigurationItemsQuery request, ConfigurationItemsResponse response)
     {
-        context.SetExpandedObjectGraph(response.CartAggregate);
+        if (response.CartAggregate is not null)
+        {
+            context.SetExpandedObjectGraph(response.CartAggregate);
+        }
+
         return Task.CompletedTask;
     }
 
