5654a22b404f1beca99dc0cb1cfd6b0dfbe3c7cb
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2025-12-27 15:00:46 +0300

    feat: Enhance CartConfigurationItemType with product field and data loader integration

--- src/VirtoCommerce.XCart.Core/Schemas/CartConfigurationItemType.cs
+++ src/VirtoCommerce.XCart.Core/Schemas/CartConfigurationItemType.cs
@@ -1,22 +1,34 @@
+using GraphQL.DataLoader;
+using GraphQL.Resolvers;
 using GraphQL.Types;
+using MediatR;
 using VirtoCommerce.CartModule.Core.Model;
+using VirtoCommerce.CoreModule.Core.Currency;
 using VirtoCommerce.Xapi.Core.Extensions;
+using VirtoCommerce.Xapi.Core.Helpers;
 using VirtoCommerce.Xapi.Core.Schemas;
 using VirtoCommerce.XCart.Core.Extensions;
+using VirtoCommerce.XCatalog.Core.Models;
+using VirtoCommerce.XCatalog.Core.Schemas;
 
 namespace VirtoCommerce.XCart.Core.Schemas
 {
     public class CartConfigurationItemType : ExtendableGraphType<ConfigurationItem>
     {
-        public CartConfigurationItemType()
+        public CartConfigurationItemType(
+            IMediator mediator,
+            IDataLoaderContextAccessor dataLoader,
+            ICurrencyService currencyService)
         {
             Field(x => x.Id, nullable: false).Description("Configuration item ID");
-            Field(x => x.Name, nullable: true).Description("Configuration item name");
             Field(x => x.SectionId, nullable: false).Description("Configuration item section ID");
+            Field(x => x.Type, nullable: false).Description("Configuration item type. Possible values: 'Product', 'Variation', 'Text', 'File'");
             Field(x => x.ProductId, nullable: true).Description("Configuration item product ID");
+            Field(x => x.Name, nullable: true).Description("Configuration item name");
+            Field(x => x.Sku, nullable: true).Description("Configuration item SKU");
+            Field(x => x.ImageUrl, nullable: true).Description("Configuration item image URL");
             Field(x => x.Quantity, nullable: true).Description("Configuration item product quantity");
             Field(x => x.CustomText, nullable: true).Description("Custom text for 'Text' configuration item section");
-            Field(x => x.Type, nullable: false).Description("Configuration item type. Possible values: 'Product', 'Variation', 'Text', 'File'");
 
             Field<NonNullGraphType<MoneyType>>("listPrice")
                 .Description("List price")
@@ -33,6 +45,15 @@ namespace VirtoCommerce.XCart.Core.Schemas
             ExtendableField<ListGraphType<CartConfigurationItemFileType>>(nameof(ConfigurationItem.Files),
                 resolve: context => context.Source.Files,
                 description: "List of files for 'File' configuration item section");
+
+            var productField = new FieldType
+            {
+                Name = "product",
+                Type = GraphTypeExtensionHelper.GetActualType<ProductType>(),
+                Resolver = new FuncFieldResolver<ConfigurationItem, IDataLoaderResult<ExpProduct>>(context =>
+                    dataLoader.LoadCartProduct(context, mediator, currencyService, "cart_configurationItems_products", context.Source.ProductId)),
+            };
+            AddField(productField);
         }
     }
 }
